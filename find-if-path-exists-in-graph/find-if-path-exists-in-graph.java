18c5f706aaf520bd07f928c5e054330f2d1ea4df
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-08-13 23:26:01 +0200

    Time: 136 ms, Memory: 186.6 MB - LeetHub

--- find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,36 +1,33 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i =0; i<n;i++ ){
+        for(int i =0;i< n;i++){
             adj.add(new ArrayList<>());
         }
         
-        for(int []edge: edges ){
+        
+        for(int[] edge : edges){
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
         }
         
-        Queue <Integer> queue = new LinkedList<>();
-        queue.add(source);
-        
-        boolean [] seen = new boolean[n];
-        
-        
-        while(!queue.isEmpty()){
-            int curr  = queue.poll();
-            if(curr == destination){
+        ArrayDeque<Integer> stack = new ArrayDeque();
+        stack.push(source);
+        boolean []seen = new boolean [n];
+        Arrays.fill(seen, false);
+        while(!stack.isEmpty()){
+            int node  = stack.pop();
+            if(node == destination){
                 return true;
             }
-            if(seen[curr]){
+            if(seen[node]){
                 continue;
             }
-            seen[curr] =true;
-            for(int num : adj.get(curr)){
-                queue.add(num);
+            seen[node] =true;
+            for(int child :adj.get(node)){
+                stack.push(child);
             }
-            
         }
-        
         return false;
         
     }

d2367b894dc46de9f9e472715272f29fbf72125c
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-08-13 16:52:35 +0200

    Time: 15 ms (73.06%), Space: 44.7 MB (54.03%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        Map<Character,Integer > map = new HashMap<>();
+        for(char ch : t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int start  = 0;
+        int size = Integer.MAX_VALUE;
+        int starString =0;
+        String word;
+        int match =0;
+        for(int end  = 0; end< s.length();end++){
+            char right =s.charAt(end);
+            if(map.containsKey(right)){
+                 map.put(right,map.get(right)-1);
+                if(map.get(right) >= 0){
+                    match++;
+                }
+            }
+
+            while(match == t.length()){
+                if(size > end -start +1){
+                    starString = start;
+                    size = end-start +1;
+                }
+                
+                
+                char left = s.charAt(start++);
+                if(map.containsKey(left)){
+                  if(map.get(left) == 0){
+                      match--;
+                  }
+                    map.put(left, map.get(left)+1);
+                }
+                
+            }
+            
+            
+            
+            
+            
+        }
+        return size > s.length()?  "" : s.substring(starString,starString+size);
+        
+        
+    }
+}

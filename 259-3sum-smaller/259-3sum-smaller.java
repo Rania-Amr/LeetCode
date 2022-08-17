a43edf22538e9a19e7361a541ff7ac6163924cd6
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-08-17 02:28:32 +0200

    Time: 9 ms (94.68%), Space: 41.6 MB (99.60%) - LeetHub

---
+++ 259-3sum-smaller/259-3sum-smaller.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int threeSumSmaller(int[] arr, int target) {
+         int count = 0;
+         Arrays.sort(arr);
+         for (int i = 0; i < arr.length - 2; i++) {
+          count += searchPair(arr, target - arr[i], i);
+        }
+        return count;
+    }
+    
+    
+    
+     private  int searchPair(int[] arr, int targetSum, int first) {
+        int count = 0;
+        int left = first + 1, right = arr.length - 1;
+        while (left < right) {
+          if (arr[left] + arr[right] < targetSum) { 
+            count += right - left;
+            left++;
+          } else {
+            right--; 
+          }
+        }
+        return count;
+  }
+
+}

4418cac0c326483bca5d95ad75134ad657d4e63a
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-08-23 05:07:53 +0200

    Time: 0 ms (100.00%), Space: 54.1 MB (43.56%) - LeetHub

---
+++ 1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean flag = false;
+    public boolean isValidSequence(TreeNode root, int[] arr) {
+       return dfs(root,arr,0);
+    }
+    public static boolean dfs(TreeNode root,int[] arr,int i) {
+      if (root == null)
+            return false ;
+    if(i == arr.length-1 && root.val == arr[i]  && root.left ==null && 
+       root.right ==null ){
+            return true;
+    }
+      if( i < arr.length && root.val == arr[i]){
+          
+          return dfs(root.left,arr,i+1) || dfs(root.right,arr,i+1);
+      }else{
+          return false ;
+      }}
+        
+}

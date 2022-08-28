8327fb52669a95763e22c28e6fdcb1ca9318fc37
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-08-28 04:00:32 +0200

    Time: 6 ms (95.09%), Space: 42.8 MB (97.68%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.java
@@ -1,18 +1,31 @@
 class Solution {
+    Map <String ,PriorityQueue< String>> paths =new  HashMap<>();
+     List<String> path =new ArrayList<>();
     public List<String> findItinerary(List<List<String>> tickets) {
-        Map<String, PriorityQueue<String>> targets = new HashMap<>();
-        for (List<String> ticket : tickets){
-            targets.computeIfAbsent(ticket.get(0), k -> new PriorityQueue()).add(ticket.get(1));
+        for(List<String> ticket : tickets){
+            if(!paths.containsKey(ticket.get(0))){
+                 paths.put(ticket.get(0),new PriorityQueue<>());
+            }
+                paths.get(ticket.get(0)).add(ticket.get(1));
+           
         }
-        List<String> route = new LinkedList();
-        Stack<String> stack = new Stack<>();
-        stack.push("JFK");
-        while (!stack.empty()) {
-            while (targets.containsKey(stack.peek()) && !targets.get(stack.peek()).isEmpty()){
-                stack.push(targets.get(stack.peek()).poll());
-        }
-                       route.add(0, stack.pop());
-        }
-        return route;
-        }
+        dfs("JFK");
+        return path;
+    }
+    
+    public void dfs(String ticket) {
+    
+      int size = paths.containsKey(ticket) ? paths.get(ticket).size() : 0;
+      for(int i = 0; i < size;i++ ){
+          String t = paths.get(ticket).poll();
+          if( t != null){
+               dfs(t);
+          }
+         
+      }
+          path.add(0,ticket);
+    }
+    
+    
+    
 }

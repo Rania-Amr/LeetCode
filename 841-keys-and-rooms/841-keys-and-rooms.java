6cd12ae525cc07ec8a4f349aff1ebd024fd4b07a
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-08-28 02:41:30 +0200

    Time: 1 ms (97.40%), Space: 44.9 MB (41.78%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.java
+++ 841-keys-and-rooms/841-keys-and-rooms.java
@@ -1,30 +1,22 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        boolean []seen =new boolean[rooms.size()];
-        Stack <Integer> stack = new Stack();
-        
-        stack.push(0);
-        seen[0] = true;
-        while(!stack.isEmpty()){
-            int curr =stack.pop();
-            for(int n : rooms.get(curr)){
-                if(!seen[n]){
-                    stack.push(n);
-                    seen[n]=true;
-                }
-            }
-        }
-        
-        
-        
-        for(boolean flag: seen){
+        boolean [] visited = new boolean [rooms.size()];
+        dfs(rooms,visited,0);
+        for(boolean flag: visited){
             if(!flag){
                 return false;
             }
         }
         return true;
     }
-    
-    
+      public void dfs(List<List<Integer>> rooms , boolean [] visited ,int i) {
+       if(visited[i]){
+           return;
+       }
+        visited[i] =true;
+         for(int n : rooms.get(i)){
+               dfs(rooms,visited,n);
+         }
+    }
     
 }

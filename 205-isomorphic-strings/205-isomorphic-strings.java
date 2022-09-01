828f57916b1a47d57a90b621b0271edcf65c7f91
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-09-01 04:24:05 +0200

    Time: 33 ms (13.11%), Space: 43.1 MB (44.32%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.java
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -1,15 +1,22 @@
 class Solution {
+  //Input: s = "paper", t = "title"
+
     public boolean isIsomorphic(String s, String t) {
-        Map <Character,Character> mapS = new HashMap<>();
-        Map <Character,Character> mapT = new HashMap<>();
-        for(int i = 0; i < t.length();i++){
-            if(mapS.containsKey(s.charAt(i)) && mapS.get(s.charAt(i)) != t.charAt(i) ||
-              mapT.containsKey(t.charAt(i)) && mapT.get(t.charAt(i)) != s.charAt(i)){
-                return false;
-            }
-            mapS.put(s.charAt(i),t.charAt(i));
-            mapT.put(t.charAt(i),s.charAt(i));
+        /*mapS p-t
+        mapT t-p
+        */
+        Map<Character ,Character> mapS =new HashMap<>();
+        Map<Character ,Character> mapT =new HashMap<>();
+        for(int i =0; i< s.length();i++){
+          char a = s.charAt(i);  
+          char b = t.charAt(i);  
+          if((mapS.containsKey(a) && mapS.get(a) !=b) || 
+             (mapT.containsKey(b) && mapT.get(b) !=a)){
+              return false;
+          }
+            mapS.put(a,b);
+            mapT.put(b,a);
         }
-        return true;
+return true;
     }
 }

3feb01cefae43aa1cfaa1b52c69183ac49128322
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-09-01 04:09:10 +0200

    Time: 1 ms (100.00%), Space: 43.2 MB (90.18%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,19 +1,40 @@
 class Solution {
+    /*
+    
+    [1,7,3,6,5,6]
+  
+    right = 28
+    loop 
+    right-arr[i]
+    i=0
+    left = 1
+    if(right ==left) {
+    return
+    }
+        left =+arr[i]
+
+
+  right = 1
+  left  =11
+  i =3
+
+    */
     public int pivotIndex(int[] nums) {
-        int leftSum = 0;
-        int sum =0;
-        for(int i = 0; i < nums.length;i++){
-            sum+=nums[i];
+        //right 27
+      int rightsum =0;
+        //left 0
+        int leftSum =0;
+        //[1,7,3,6,5,6]
+        for(int i =0;i< nums.length;i++){
+            rightsum+=nums[i];
         }
-        
-      for(int i = 0; i< nums.length;i++){
-          if(leftSum == (sum- leftSum-nums[i])){
-              return i;
-          }
+        for(int i =0;i< nums.length;i++){
+            rightsum-=nums[i];
+            if(rightsum ==leftSum ){
+                return i;
+            }
              leftSum+=nums[i];
-      }
-            return -1;
-        
-        
+        }
+        return -1;
     }
 }

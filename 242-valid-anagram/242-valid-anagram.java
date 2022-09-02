5ca1246dde3bd299e96bb3cb2993e6079ce89ad7
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-09-03 00:41:26 +0200

    Time: 16 ms (29.65%), Space: 46.5 MB (18.11%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,19 +1,32 @@
 class Solution {
+    /**
+    Input: s = "anagram", t = "nagaram"
+Output: true
+a 3
+n 1
+g 1
+ r 1   
+    
+    m 1
+    */
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()){
             return false;
         }
-        int [] alph = new int[26];
-        for(int i = 0; i< s.length(); i++){
-            alph[s.charAt(i)-'a']++;
-            alph[t.charAt(i)-'a']--;
+        Map <Character ,Integer> map = new HashMap<>();
+        for(int i =0;i< s.length();i++){
+            char ch = s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
-        for(int i = 0; i< alph.length; i++){
-            if(alph[i] != 0){
-                return false;
-            }
+         for(int i =0;i< t.length();i++){
+            char ch = t.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)-1);
+        }
+        for(Map.Entry<Character ,Integer> entry :map.entrySet() ){
+          if(entry.getValue() !=0){
+              return false;
+          }
         }
         return true;
-        
     }
 }

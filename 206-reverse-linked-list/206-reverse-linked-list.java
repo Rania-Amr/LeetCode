242d200f8c10cb9067085d4c995a5c131d4af9ee
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-09-04 01:36:20 +0200

    Time: 0 ms (100.00%), Space: 42.7 MB (67.19%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -8,15 +8,44 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/**
+1 -> 2-> 3-> 4-> 5->null
+p2 =null
+
+---loop on null
+l1 1 
+p1  =l1.next
+l1.next =p2
+
+p2 = l1
+l1=p1
+->l1
+
+l1 2
+p1-> 2
+p1.next =null
+ p2 = 1 
+
+1 ->null
+
+5 ->4->3->2->1 -> null
+
+
+
+*/
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null ||head.next == null){
-            return head;
-        }
-       
-        ListNode temp = reverseList(head.next);
-       head.next.next = head;
-        head.next = null;
-        return temp;
+       ListNode l1 =head;
+        ListNode prev=null;
+        
+            while(l1 !=null){
+              ListNode NextValue =l1.next;
+                l1.next =prev;
+                prev =l1;
+                l1 = NextValue;
+                
+            }
+        return prev;
     }
 }

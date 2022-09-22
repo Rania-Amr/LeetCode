8d3e38dd6c1d8af475d3bbfde89b602bcb6a90aa
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-09-22 02:53:21 +0200

    Time: 0 ms (100.00%), Space: 42.6 MB (60.71%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,34 +1,45 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = matrix.length;
-        int col = matrix[0].length;
+        //bianry on roew 
+        //binary of col
         
-        int up = 0;
-        int down = matrix.length-1;
-        while(up <= down){
-            int medRow  = up+(down-up)/2;
-            if(matrix[medRow][0] < target && matrix[medRow][col-1] < target){
-               up = medRow+1; 
-            }else if (matrix[medRow][0] > target){
-                down = medRow-1;
-            }else{
-                up =down=medRow;
-                int right = 0;
-                int left = col-1;
-                while(right <= left){
-                    int medCol = right+(left-right)/2;
-                    if(matrix[medRow][medCol] > target){
-                        left = medCol-1;
-                    }else if(matrix[medRow][medCol] < target){
-                        right = medCol+1;
-                    }else{
-                        return true;
-                    }
-                        
-                }
-                return false;
-            }
+    int row = binarySearchRow(matrix,  target);
+        if(row != -1){
+            int col =binarySearchCol(matrix,  target,row);
+            return col ==-1?false :true;
         }
         return false;
+    }
+    public int binarySearchRow(int[][] matrix, int target) {
+       int start =0;
+        int end = matrix.length-1;
+        int n =matrix[0].length-1;
+        while(start <=end){
+            int mid =start+(end-start)/2;
+            if(target >=matrix[mid][0] &&  matrix[mid][n] >=target){
+                return mid;
+            }else if (matrix[mid][n] <target){
+                start =mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
+    }
+      public int binarySearchCol(int[][] matrix, int target,int row) {
+       int start =0;
+        int end = matrix[row].length-1;
+  
+        while(start <=end){
+            int mid =start+(end-start)/2;
+            if(target ==matrix[row][mid]){
+                return mid;
+            }else if (matrix[row][mid] <target){
+                start =mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
     }
 }

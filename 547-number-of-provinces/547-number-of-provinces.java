b7c9acf8e267db764bbd8892b6a95051459d1ec9
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-10-02 05:42:38 +0200

    Time: 6 ms (29.91%), Space: 54.7 MB (12.39%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,63 +1,59 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        if (isConnected == null || isConnected.length == 0) {
-            return 0;
-        }
-
-        int n = isConnected.length;
-        UnionFind u = new UnionFind(n);
-        for(int  i= 0; i< isConnected.length; i++){
-            for(int j =0;j<isConnected.length;j++){
-                if(isConnected[i][j] == 1){
-                    u.union(i,j);
-                }
-            }
-            
-        }
-        
-       return u.getCount(); 
-    }
-    
-    
-    class UnionFind{
-        int [] rank;
+    class unionFind{
         int [] root;
+        int [] rank ;
         int count ;
-        public UnionFind(int n){
+       public unionFind(int n){
+            root = new int [n];
             rank = new int [n];
-            root =new int [n];
-            count = n;
-            for(int i =0; i<n;i++){
-                root[i]=i;
-                rank[i] = 1;
-            }
+           count =n;
+           for(int i =0;i<n;i++){
+               root[i] = i;
+               rank[i] = 1;
+           }
         }
         
-        public int find (int x){
-            if(root[x] == x){
-                return x;
-            }
-           return  root[x] =find(root[x]);
-        }
-         public int getCount() {
-             return count;
-         }
-        
-        public void union(int x, int y){
+        public void union(int x,int y){
             int rootX = find(x);
             int rootY = find(y);
-            if(rootX != rootY){
+            if(rootX !=rootY){
+                count--;
                 if(rank[rootX] > rank[rootY]){
                     root[rootY] = rootX;
                 }else if(rank[rootX] < rank[rootY]){
-                    root[rootX] = rootY;
+                     root[rootX] = rootY;
                 }else{
-                    root[rootY] = rootX;
+                     root[rootY] = rootX;
                     rank[rootX] += 1;
                 }
-                count--;
             }
-            
         }
+        public int getCount(){
+            return count;
+        }
+      public int find (int x){
+             if(x == root[x]){
+                 return x;
+             }
+            return root[x] = find(root[x]);
+         }
+        public boolean isConeccted(int x,int y){
+            return find(x) ==find(y);
+        }
+        
+    }
+    public int findCircleNum(int[][] isConnected) {
+        if(isConnected == null || isConnected.length ==0){
+            return 0;
+        }
+        unionFind uf=new unionFind(isConnected.length);
+        for(int i =0;i<isConnected.length;i++){
+            for(int j =0; j< isConnected[i].length;j++){
+                if(isConnected[i][j] == 1){
+                    uf.union(i,j);
+                }
+            }
+        }
+        return uf.getCount();
     }
 }

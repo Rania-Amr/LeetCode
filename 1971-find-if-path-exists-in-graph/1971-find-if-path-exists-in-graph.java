369957d23f4b0ed6f803c7c945bd9e98fe7dbca0
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-12-13 00:16:12 +0200

    Time: 190 ms (57.26%), Space: 214 MB (42.55%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,37 +1,30 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-     List <List <Integer>> adj =new ArrayList<>();
-        for(int i =0; i< n;i++){
+       List<List<Integer>> adj= new ArrayList<>();
+        for(int i =0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] edge : edges ){
+        for(int [] edge : edges){
             adj.get(edge[0]).add(edge[1]);
             adj.get(edge[1]).add(edge[0]);
+
         }
-        boolean [] visited = new boolean[n];
-        Arrays.fill(visited,false);
-        ArrayDeque<Integer> stack  =new ArrayDeque<>();
-        stack.addFirst(source);
-        while(!stack.isEmpty()){
-            int num =stack.removeFirst();
-            if(num == destination){
-                return true;
+        boolean visit [] =new boolean[n];
+        return dfs(visit,adj,source,destination);
+    }
+    public boolean  dfs(boolean [] visit, List<List<Integer>>  adj, int source,int destination){
+        if( source == destination){
+            return true;
+        }
+        visit[source] = true;
+        for(int n:adj.get(source)){
+            if( visit[n] ){
+                continue;
             }
-            visited[num] =true;
-            for(int child: adj.get(num)){
-                if(!visited[child]){
-                    stack.addFirst(child);
-                }
+            if( dfs(visit,adj,n,destination)){
+                return true;
             }
         }
         return false;
     }
-    
-    /*
-    
-    
-    
-    
-    
-    */
 }

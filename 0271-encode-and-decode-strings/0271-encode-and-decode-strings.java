b3d016f0be4f339a9876e4f3724ea3b741b18d59
Rania Amr <46952073+Rania-Amr@users.noreply.github.com>
2022-12-29 21:40:36 +0200

    Time: 4 ms (99.04%), Space: 43.2 MB (80.54%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -0,0 +1,29 @@
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+          sb.append(str.length()).append("#").append(str);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> strs = new ArrayList<>();
+        int i = 0;
+        while(i < s.length()){
+            int j = i;
+            while(j < s.length() && s.charAt(j) !='#')j++;
+            int length =Integer.valueOf(s.substring(i,j));
+            i = j+1+length;
+            strs.add(s.substring(j+1, i));
+        }
+        return strs;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
